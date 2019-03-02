b3123ddcda3c6802b120e6fe72801f801697cd35
Robert Rice <rrice1291@gmail.com>
2019-03-02 13:51:54 -0600

    classes and stuff for first weekend class in c#

---
+++ ClassesExamples/Candy.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassesExamples
+{
+    class Candy
+    {
+        //Properties
+        public string Flavor { get; set; }
+        public int Rating { get; set; }
+        public string Review { get; set; }
+        public string Name { get; set; }
+        public CandyType Type { get; set; }
+
+        //Fields
+        const int MinRatingValue = 1;
+        const int MaxRatingValue = 10;
+
+        //Consctructor
+        public Candy(string flavor, string name, CandyType type)
+        {
+            Flavor = flavor;
+            Name = name;
+            Type = type;
+        }
+
+        //Methods
+        public void SetRating(int userRating)
+        {
+            if(userRating >= MinRatingValue && userRating <= MaxRatingValue)
+            {
+                Rating = userRating;
+            }
+            else
+            {
+                throw new Exception($"You are dumb. Ratings are {MinRatingValue}-{MaxRatingValue}. You moron.");
+            }
+        }
+        public void SetRating(int userRating, string review)
+        {
+            SetRating(userRating);
+            Review = review;
+        }
+        public override string ToString()
+        {
+            return $"This is {Type} {Name}, with a(n) {Flavor} flavor, rated {Rating} with a review of {Review}";
+        }
+
+    }
+    enum CandyType
+    {
+        Chocolate,
+        HardCandy,
+        Caramel,
+        Sour,
+        Stretchy,
+        Nut
+    }
+}
--- ClassesExamples/Program.cs
+++ ClassesExamples/Program.cs
@@ -4,9 +4,15 @@ namespace ClassesExamples
 {
     class Program
     {
-        static void Main(string[] args)
+
+        private static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var taffy = new Candy("Orange", "Orange Saltwater Taffy", CandyType.Stretchy);
+            taffy.SetRating(10);
+            taffy.SetRating(5, "Just kinda liked it.");
+
+            Console.WriteLine(taffy);
+            Console.ReadLine();
         }
     }
 }
